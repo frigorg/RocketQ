bcdc89c59616f7bc47b4fa1405a6c038a97fac31
rafael frigo <rafaelfrigo@protonmail.com>
2022-04-14 14:29:17 -0300

    action buttons from room page working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,25 @@
-import { Modal } from './modal'
+import Modal from './modal.js'
+
+const modal = Modal();
+
+const unread_cards_trashbtn = document.querySelectorAll(".card_unread .actions .trash");
+const unread_cards_readbtn = document.querySelectorAll(".card_unread .actions .read");
+
+unread_cards_trashbtn.forEach((trashbtn)=>{
+    trashbtn.addEventListener("click", e => {
+        modal.changeToDeleteModal();
+        modal.open()
+    })
+})
+
+unread_cards_readbtn.forEach((readbtn)=>{
+    readbtn.addEventListener("click", e => {
+        modal.changeToReadModal();
+        modal.open()
+    })
+})
+
+const modal_wrapper = document.querySelector(".modal-wrapper")
+
+modal_wrapper.querySelector(".btn.cancel").addEventListener("click", e => {modal.close()})
+modal_wrapper.querySelector(".btn.proceed").addEventListener("click", e => {modal.close()})
--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,14 +1,30 @@
-function Modal(){
-    function open(){
+export default function Modal(){
 
+    const modal_wrapper = document.querySelector(".modal-wrapper");
+
+    function open(){
+        modal_wrapper.classList.remove("hidden");
     }
     function close() {
+        modal_wrapper.classList.add("hidden");
+    }
+    function changeToDeleteModal() {
+        modal_wrapper.querySelector("h2").innerHTML = "Excluir pergunta";
+        modal_wrapper.querySelector("p").innerHTML = "Tem certeza que voçê deseja excluir esta pergunta?";
+        modal_wrapper.querySelector(".proceed").innerHTML = "Sim, excluir";
 
+    }
+    function changeToReadModal() {
+        modal_wrapper.querySelector("h2").innerHTML = "Confirmar leitura";
+        modal_wrapper.querySelector("p").innerHTML = "Tem certeza que voçê deseja marcar esta pergunta como lida?";
+        modal_wrapper.querySelector(".proceed").innerHTML = "Sim, marcar";
     }
 
     return {
         open,
-        close
+        close,
+        changeToDeleteModal,
+        changeToReadModal
     }
     
 } 
